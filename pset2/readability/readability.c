c3ef20f3d0b9c824aca5d1c5e71edb8fbf348b75
enochgeorge9x9 <enochgeorge1999@gmail.com>
2020-09-13 21:30:11 +0300

    Readability final changes

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -1,40 +1,80 @@
 #include <stdio.h>
-#include <cs50.c>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
-int count_letter(char *text);
-int count_word(char *word);
+int count_letter(char text[]);
+long count_word(char word[]);
+long count_sentence(char text[]);
 
 int main(void)
 {
-    char *text= "It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him.";
-    // string text = get_string("Text: ");
+    char text[500];
+    do
+    {
+        printf("Text: ");
+        fgets(text, sizeof(text), stdin);
+    } while (strlen(text) - 1 == 0);
+
+    //*letters words and sentences count
     int letter = count_letter(text);
-    int word = count_word(text);
-    printf("%i letters(s)\n", letter);
+    long word = count_word(text);
+    long sentences = count_sentence(text);
+
+    //*Calculating the Grade level index
+    float L = (letter / (float)word) * 100;
+    float S = ((float)sentences / word) * 100;
+    float index = round((0.0588 * L) - (0.296 * S) - 15.8);
+
+    //*Before Grade 1 and Grade 16+
+    if (index <= 1)
+    {
+        printf("Before Grade 1");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+");
+    }
+    else
+    {
+        printf("Grade %.0f\n", index);
+    }
 }
 
-int count_word(char *word)
+//*Calculating how many sentences are there in a paragraph
+long count_sentence(char text[])
 {
-    int count = 0;
-    for(int i = 0,n = strlen(word); i < n; i++)
+    long count = 0;
+    for (long i = 0, n = strlen(text); i < n; i++)
     {
-        if(isalnum(word[i] != 0))
+        if (text[i] == 33 || text[i] == 46 || text[i] == 63)
         {
             count += 1;
         }
     }
-    printf("word: %i\n", count);
     return count;
 }
 
-int count_letter(char *letter)
+//*Calculating how many words are there in senternce
+long count_word(char word[])
+{
+    long count = 1;
+    for (long i = 0, n = strlen(word); i < n; i++)
+    {
+        if (word[i] == 32)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int count_letter(char letter[])
 {
     int count = 0;
-    for(int i = 0,n = strlen(letter); i < n; i++)
+    for (int i = 0, n = strlen(letter); i < n; i++)
     {
-        if(isalnum(letter[i]))
+        if (isalnum(letter[i]))
         {
             count += 1;
         }
