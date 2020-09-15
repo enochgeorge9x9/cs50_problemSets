02df059f755022e1711331fcf65cf1e66feeaa52
enochgeorge9x9 <enochgeorge1999@gmail.com>
2020-09-15 23:57:42 +0300

    Done with Subsitution Program in pset2

--- pset2/substitution/pseudocode.md
+++ pset2/substitution/pseudocode.md
@@ -5,10 +5,10 @@
         [X]Check if its 26 characters
         [X]Check if there are no repeats in key alphabets (case-insensitive)
 [X]Prompt User for a plaintext
-[-]Encipher the plaintext with the key
-    []Map each alphabetic A-Z && a-z with the key given case sensitive
-    []Preserve case.
-    []Iterate over plaintext
-        []If non-alphabetic charater (keys its as it is)
-        []Replace the aplabets with the keys given
-[]Print the ciphertext
+[X]Encipher the plaintext with the key
+    [X]Map each alphabetic A-Z && a-z with the key given case sensitive
+    [X]Preserve case.
+    [X]Iterate over plaintext
+        [X]If non-alphabetic charater (keys its as it is)
+        [X]Replace the aplabets with the keys given
+[X]Print the ciphertext
--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -4,68 +4,81 @@
 #include <cs50.h>
 
 bool validate_key(char arg[]);
+void array_val(char pos, string key);
 
-//* int main(int argc, char *argv[])
-int main(void)
+int main(int argc, char *argv[])                  
 {
     //*Checking if the key is valide or not
-
-    //!dont need for the final script
-    char *argv[200] = {"ji", "VCHPRZGJNTLSKFBDQWAXEUYMOI"};
-    int argc = 2;
-    //!Nope dont need this in final output
-
-    if (strlen(argv[1]) != 26)
-    {
-        printf("Key must contain 26 characters.\n");
-    }
-
-    else if (argc != 2 || !validate_key(argv[1]))
+    if(argc != 2 || !validate_key(argv[1]))
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
+    if (strlen(argv[1]) != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+    
+
+    string key = argv[1];
 
     //*Getting the User Prompt
-    // char text[500];
-    // do
-    // {
-    //     printf("plaintext: ");
-    //     fgets(text, sizeof(text), stdin);
-    // } while (strlen(text) - 1 == 0);
-
-    //!Temp
-    string text = "hello, world";
-    printf("plaintext: %s\n", (string) text);
-
-    //*Encrypt the plaintext
-    string ciphertext = text;
-    string key = argv[1];
-    printf("%s\n%s", ciphertext,key);
-    for(int i = 0, len = strlen(text); i < len; i++)
+    char text[500];
+    do
     {
-        if(isalpha(text[i]))
+        printf("plaintext: ");
+        fgets(text, sizeof(text), stdin);
+    } while (strlen(text) - 1 == 0);
+
+
+    //*Encrpytion
+    printf("ciphertext: ");
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (isalpha(text[i]))
         {
-            if(isupper(text[i]))
+            char x = text[i];
+            if (islower(text[i]))
             {
-                 ciphertext[i] = key[text[i] - 'A'] + 'A';
+                array_val(tolower(x), key);
             }
             else
             {
-                 ciphertext[i] = key[text[i] - 'a'] + 'a';   
+                array_val(toupper(x), key);
             }
-            
+        }
+        else
+        {
+            printf("%c", text[i]);
         }
     }
-
-    printf("ciphertext: %s", ciphertext);
-
-    
-
+    printf("\n");
 }
 
+//*Mapping array with given user string
+void array_val(char pos, string key)
+{
+    string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
+    for (int i = 0, len = strlen(alpha); i < len; i++)
+    {
+        if (islower(pos))
+        {
+            if (pos == tolower(alpha[i]))
+            {
+                printf("%c", tolower(key[i]));
+            }
+        }
+        else
+        {
+            if (pos == toupper(alpha[i]))
+            {
+                printf("%c", toupper(key[i]));
+            }
+        }
+    }
+}
 
 //*Validating the key is given is corrent
 bool validate_key(char arg[])
